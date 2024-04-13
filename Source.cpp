ec670dab37b1c56ce0d15d2ccac4cbc59afb31a7
schwyzz <myemail@email.com>
2024-04-13 13:56:38 +0300

    new stable

--- Source.cpp
+++ Source.cpp
@@ -5,18 +5,17 @@
 #include <stdlib.h>
 #include <cstdlib>
 #include <iostream>
-//#include <conio.h> // Это в линукс visual studio code не работает
+///#include <conio.h>
 using namespace std;
 int main() {
 	setlocale(LC_ALL, "");
-	printf("  Hello Dolly!\n Привет Максим как дела и как там вообще чо.\n \n  Test fixing.\n");
-	printf(" Hello Dolly! Это тест линукс visual studio code на предмет кодировки текста и ещё фиксаций.\n\n");
-	printf(" Hello Dolly! Это тест windows utf-8 with BOM., проверка.");
-	cout << " Hello Dolly!\nВсё работает и кодировка читается как принял: 'utf-8 with BOM'.\n";
-	//_getch(); // Это в линукс visual studio code не работает
+	printf("  Hello Dolly!\n Максим привет как дела и как там вообще чо.");
+	///getch();
 	std::cin.get();
-	cin.get();
-	scanf("d"); // здесь вместо  scanf_s так как на линуксе надо scanf
-	system("pause");
+	///scanf("d");
+	cout << "\nМаксим, для выхода нажми Enter\n";
+	scanf("f");
+	///cin.get();
+	///system("pause");
 	return 0;
 }
