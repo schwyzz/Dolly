0667482dec3763106b8e3fffdfe5935dfc9ddb84
schwyzz <myemail@email.com>
2024-04-14 04:59:10 +0300

    Dolly v.1.5 release

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,7 @@
 ﻿#include <stdio.h>
 #include <locale.h>
 #include <iostream>
-#include <conio.h>
+#include <conio.h> // github.com/zoelabbb/conio.h/releases/tag/Version
 int main() {
 	setlocale(LC_ALL, "");
 	printf("\n   Hello Dolly!\n  Максим привет как дела и как там вообще чо.\n\n");
